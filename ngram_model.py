a51ca54ce5ad6f65c63ae39a894281327623cac6
CodingYodha <codingyodha7706@gmail.com>
2025-03-31 11:49:39 +0530

    define train function

--- ngram_model.py
+++ ngram_model.py
@@ -1,10 +1,21 @@
+import re
+from collections import defaultdict
+from preprocess import preprocess
+
 class NGramModel:
     def __init__(self, n=3):
         self.n = n
-        self.ngrams = defaultdict(lamda: defaultdict(int))
+        self.ngrams = defaultdict(lambda: defaultdict(int))
         self.vocab = set()
 
     def train(self, corpus):
         #collect n-grams and vocabulary
         for sentence in corpus:
             tokens = preprocess(sentence)
+            self.vocab.update(tokens)
+            for i in range(len(tokens)-self.n+1):
+                context = tuple(tokens[i:i+self.n-1])
+                next_word = tokens[i+self.n-1]
+                self.ngrams[context][next_word]+=1
+            
+        

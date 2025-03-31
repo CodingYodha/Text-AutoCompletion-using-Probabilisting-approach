452611bd08c083989826c4f5533dacc3c5f1e1dc
CodingYodha <codingyodha7706@gmail.com>
2025-03-31 11:52:26 +0530

    define laplace smoothing

--- ngram_model.py
+++ ngram_model.py
@@ -17,5 +17,15 @@ class NGramModel:
                 context = tuple(tokens[i:i+self.n-1])
                 next_word = tokens[i+self.n-1]
                 self.ngrams[context][next_word]+=1
+
+        #add laplace smoothing
+        self._apply_smoothing()
+
+    def _apply_smoothing(self, alpha=0.1):
+        vocab_size = len(self.vocab)
+        for context in self.ngrams:
+            total = sum(self.ngrams[context].values())+alpha*vocab_size
+            for word in self.vocab:
+                self.ngrams[context][word] = (self.ngrams[context].get(word, 0) + alpha)/total
             
         
